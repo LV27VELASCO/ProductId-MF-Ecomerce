41a12e89796313ad181fc324098c590b7cdb6067
Luis Velasco <luiselsw27@gmail.com>
2023-06-19 21:45:16 -0500

    .

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import "../styles/style.css"
-import { Outlet } from 'react-router-dom'
 const Header = () => {
   return (
     <div className="bg-blue-700 h-14 mb-10 flex justify-center items-center text-lg">Header</div>
